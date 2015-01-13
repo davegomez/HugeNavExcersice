49a6bb21f73b8bd7faf29c912276155a477448ee
David Gomez <dg.dave@gmail.com>
2015-01-13 16:25:55 -0500

    Gulp configuration to rename and minify resulted js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		uglify = require('gulp-uglify'),
+		rename = require('gulp-rename'),
+		concat = require('gulp-concat'),
+		karma = require('karma').server;
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'compress', 'test', 'watch']);
 
 gulp.task('sass', function () {
 	gulp.src(['src/sass/**/*.scss'])
@@ -12,6 +15,23 @@ gulp.task('sass', function () {
 			.pipe(gulp.dest('public/styles'));
 });
 
+gulp.task('compress', function() {
+  gulp.src('src/js/scripts.js')
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
 gulp.task('watch', function () {
-	gulp.watch('public/styles/sass/*.scss', ['sass'])
+	gulp.watch('public/styles/sass/*.scss', ['sass']);
+	gulp.watch('src/js/scripts.js', ['compress']);
+	gulp.watch('spec/**/*.js', ['test']);
+	gulp.watch('src/js/**/*.js', ['test']);
 });
