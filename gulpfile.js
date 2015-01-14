e1c37e0fe244027df36b13473375424edb4ee21e
David Gomez <dg.dave@gmail.com>
2015-01-14 11:28:20 -0500

    Upgrade configuration files for task and test runners

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,44 @@
-var gulp = require('gulp'),
-		sass = require('gulp-sass'),
-		uglify = require('gulp-uglify'),
-		rename = require('gulp-rename'),
-		concat = require('gulp-concat'),
-		karma = require('karma').server;
+var gulp 				= require('gulp'),
+		sass 				= require('gulp-sass'),
+		uglify 			= require('gulp-uglify'),
+		rename 			= require('gulp-rename'),
+		concat 			= require('gulp-concat-util'),
+		karma				= require('karma').server;
 
-gulp.task('default', ['sass', 'compress', 'test', 'watch']);
+gulp.task('default', ['sass', 'concat:dist', 'compress', 'tdd', 'watch']);
 
 gulp.task('sass', function () {
-	gulp.src(['src/sass/**/*.scss'])
+	gulp.src(['src/sass/{,*/}*.scss'])
 			.pipe(sass())
 			.pipe(gulp.dest('public/styles/css'))
 			.pipe(concat('styles.css'))
 			.pipe(gulp.dest('public/styles'));
 });
 
+gulp.task('concat:dist', function() {
+  // gulp.src(['src/js/ajax.js', 'src/js/nav-builder.js', 'src/js/init.js'])
+  gulp.src('src/js/{,*/}*.js')
+    .pipe(concat('scripts.js', {process: function(src) { return (src.trim() + '\n').replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1'); }}))
+    .pipe(concat.header('(function (window, document, undefined) {\n\'use strict\';\n\n'))
+    .pipe(concat.footer('\n})(window, document);\n'))
+    .pipe(gulp.dest('public/scripts'));
+});
+
 gulp.task('compress', function() {
-  gulp.src('src/js/scripts.js')
+  gulp.src('public/scripts/scripts.js')
     .pipe(uglify())
     .pipe(rename('scripts.min.js'))
     .pipe(gulp.dest('public/scripts'));
 });
 
-gulp.task('test', function (done) {
+gulp.task('tdd', function (done) {
   karma.start({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
+    configFile: __dirname + '/karma.conf.js'
   }, done);
 });
 
 gulp.task('watch', function () {
 	gulp.watch('public/styles/sass/*.scss', ['sass']);
-	gulp.watch('src/js/scripts.js', ['compress']);
-	gulp.watch('spec/**/*.js', ['test']);
-	gulp.watch('src/js/**/*.js', ['test']);
+	gulp.watch('src/js/{,*/}*.js', ['concat:dist']);
+	gulp.watch('public/scripts/scripts.js', ['compress']);
 });
