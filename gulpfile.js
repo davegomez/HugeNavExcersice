90a51c6cdc78da6b2c7b3ce19684d5fdd76c3f7f
David Gomez <dgomez@hugeinc.com>
2015-01-22 14:55:50 -0500

    Export documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,7 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat-util'),
-    karma = require('karma').server;
+    jsdoc = require("gulp-jsdoc");
 
-gulp.task('default', ['sass', 'concat:dist', 'compress', 'watch']);
+gulp.task('default');
 
-gulp.task('sass', function() {
-  gulp.src(['src/sass/{,*/}*.scss'])
-      .pipe(sass())
-      .pipe(gulp.dest('public/styles/css'))
-      .pipe(concat('styles.css'))
-      .pipe(gulp.dest('public/styles'));
-});
-
-gulp.task('concat:dist', function() {
-  // gulp.src(['src/js/get-json.js', 'src/js/menu-builder.js', 'src/js/init.js'])
-  gulp.src('src/js/{,*/}*.js')
-    .pipe(concat('scripts.js', {process: function(src) { return (src.trim() + '\n').replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1'); }}))
-    .pipe(concat.header('(function (window, document, undefined) {\n\'use strict\';\n\n'))
-    .pipe(concat.footer('\n})(window, document);\n'))
-    .pipe(gulp.dest('public/scripts'));
-});
-
-gulp.task('compress', function() {
-  gulp.src('public/scripts/scripts.js')
-    .pipe(uglify())
-    .pipe(rename('scripts.min.js'))
-    .pipe(gulp.dest('public/scripts'));
-});
-
-gulp.task('tdd', function(done) {
-  karma.start({
-    configFile: __dirname + '/karma.conf.js'
-  }, done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch('src/sass/{,*/}*.scss', ['sass']);
-  gulp.watch('src/js/{,*/}*.js', ['concat:dist']);
-  gulp.watch('public/scripts/scripts.js', ['compress']);
-});
+gulp.src(["./src/js/dropdown-menu.js", "./src/js/menu-builder.js"])
+  .pipe(jsdoc('./docs'));
